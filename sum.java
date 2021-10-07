c17190da20031b6ad3957ceca964d6fae5052b0c
bhagavathkonakalla1278 <74002992+bhagavathkonakalla1278@users.noreply.github.com>
2021-10-07 08:32:17 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -3,8 +3,8 @@ public class Sum {
    public static void main(String[] args) {
         
       int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+      sum = num1 + num2
 
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sum of these numbers: "+sum)
    }
 }
